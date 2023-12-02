29e59265edac94ee2ef9384efa270b42838d8eae
mitalee18 <mitaleeminde@gmail.com>
2023-12-01 23:24:51 -0500

    completed modularity and testability

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -7,7 +7,6 @@ import java.util.List;
 /**
  * The ExpenseTrackerModel class represents a model which can add, remove, get transactions as well as
  * setMatchedFilterIndices and getMatchedFilterIndices for filtering the transactions which are stored in the transaction object
- *
  * This class follows an observable design pattern and notifies the listeners whenever state is changed
  *
  * @author [Heather Conboy]
@@ -20,12 +19,15 @@ public class ExpenseTrackerModel {
   private List<Transaction> transactions;
   private List<Integer> matchedFilterIndices;
 
+  private List<ExpenseTrackerModelListener> observers;
+
   // This is applying the Observer design pattern.                          
   // Specifically, this is the Observable class. 
     
   public ExpenseTrackerModel() {
     transactions = new ArrayList<Transaction>();
     matchedFilterIndices = new ArrayList<Integer>();
+    observers = new ArrayList<ExpenseTrackerModelListener>();
   }
 
   public void addTransaction(Transaction t) {
@@ -36,12 +38,14 @@ public class ExpenseTrackerModel {
     transactions.add(t);
     // The previous filter is no longer valid.
     matchedFilterIndices.clear();
+    stateChanged();
   }
 
   public void removeTransaction(Transaction t) {
     transactions.remove(t);
     // The previous filter is no longer valid.
     matchedFilterIndices.clear();
+    stateChanged();
   }
 
   public List<Transaction> getTransactions() {
@@ -62,6 +66,7 @@ public class ExpenseTrackerModel {
       // For encapsulation, copy in the input list 
       this.matchedFilterIndices.clear();
       this.matchedFilterIndices.addAll(newMatchedFilterIndices);
+      stateChanged();
   }
 
   public List<Integer> getMatchedFilterIndices() {
@@ -83,35 +88,53 @@ public class ExpenseTrackerModel {
       // For the Observable class, this is one of the methods.
       //
       // TODO
+      if(listener != null){
+         if(containsListener(listener)){
+             return false; //if already registered return False
+         }
+          observers.add(listener);
+          return true; //if not already registered the register and return False
+      }
       return false;
   }
 
     /**
-     * This is for testing the number of listners
-     * @return 0
+     * This is for testing the number of listeners
+     * @return size of observers Arraylist
      */
   public int numberOfListeners() {
       // For testing, this is one of the methods.
       //
       //TODO
-      return 0;
+      return observers.size();
   }
 
     /**
      * This is for testing purpose
      * @param listener The ExpenseTrackerModelListener to contain listener
-     * @return False
+     * @return True if listener exists in the observers ArrayList else returns False
      */
   public boolean containsListener(ExpenseTrackerModelListener listener) {
       // For testing, this is one of the methods.
       //
       //TODO
+      for(ExpenseTrackerModelListener observer: observers){
+          if(observer.equals(listener)){
+              return true;
+          }
+      }
       return false;
   }
 
+    /**
+     * Calls update method for every observer in the arraylist to update the view
+     */
   protected void stateChanged() {
       // For the Observable class, this is one of the methods.
       //
       //TODO
+      for(ExpenseTrackerModelListener observer: observers){
+          observer.update(this);
+      }
   }
 }
